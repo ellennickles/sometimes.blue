e81809b6bfa8c41976292654e9b8af5bf32d2700
ellennickles <emn200@nyu.edu>
2022-04-30 14:31:13 -0400

    refactor myColors

--- docs/script.js
+++ docs/script.js
@@ -6,76 +6,38 @@ ellen.town
 */
 
 const myColors = [
-    {
-      num: "0",
-      color: "hsla(240, 100%, 99%, 0.95)", // ghostwhite
-    },
-    {
-      num: "1",
-      color: "hsla(60, 100%, 97%, 0.95)", // ivory
-    },
-    {
-      num: "2",
-      color: "hsla(240, 100%, 50%, 0.95)", // blue
-    },
-    {
-      num: "3",
-      color: "hsla(39, 100%, 50%, 0.95)", // orange
-    },
-    {
-      num: "4",
-      color: "hsla(0, 100%, 50%, 0.95)", // red
-    },
-    {
-      num: "5",
-      color: "hsla(271, 76%, 53%, 0.95)", // blueviolet
-    },
-    {
-      num: "6",
-      color: "hsla(300, 100%, 50%, 0.95)", // magenta
-    },
-    {
-      num: "7",
-      color: "hsla(120, 100%, 27%, 0.95)", // green4
-    },
-    {
-      num: "8",
-      color: "hsla(33, 100%, 50%, 0.95)", // darkorange
-    },
-    {
-      num: "9",
-      color: "hsla(348, 83%, 47%, 0.95)", // crimson
-    },
-  ];
-
+  "hsla(240, 100%, 99%, 0.95)", // 0 ghostwhite
+  "hsla(60, 100%, 97%, 0.95)",  // 1 ivory
+  "hsla(240, 100%, 50%, 0.95)", // 2 blue
+  "hsla(39, 100%, 50%, 0.95)",  // 3 orange
+  "hsla(0, 100%, 50%, 0.95)",   // 4 red
+  "hsla(271, 76%, 53%, 0.95)",  // 5 blueviolet
+  "hsla(300, 100%, 50%, 0.95)", // 6 magenta
+  "hsla(120, 100%, 27%, 0.95)", // 7 green4
+  "hsla(33, 100%, 50%, 0.95)",  // 8 darkorange
+  "hsla(348, 83%, 47%, 0.95)",  // 9 crimson
+];
 
 function getCurrentTime() {
-    const day = new Date();
-    const time = day.toLocaleTimeString('en-US');
-    let digits = time.match(/\d+/g);
-    digits = digits.join("");
- 
-    if (digits.length > 5) {
-        return digits.substring(0, 4);
-    } else {
-        return digits.substring(0, 3); 
-    }
-}
+  const day = new Date();
+  const time = day.toLocaleTimeString('en-US');
+  let digits = time.match(/\d+/g);
+  digits = digits.join("");
 
+  if (digits.length > 5) return digits.substring(0, 4);
+  else return digits.substring(0, 3); 
+}
 
 function getColors() {
-  const numbers = getCurrentTime();
-  let colors = [];
-  for (let i = 0; i < numbers.length; i++) {
-    for (let c = 0; c < myColors.length; c++) {
-      if (numbers[i] === myColors[c].num) {
-        colors[i] = myColors[c].color;
-      }
-    }
+const numbers = getCurrentTime();
+let colors = [];
+for (let i = 0; i < numbers.length; i++) {
+  for (let c = 0; c < myColors.length; c++) {
+    if (numbers[i] == c) colors[i] = myColors[c];
   }
-  return colors;
 }
-
+return colors;
+}
 
 function updateDisplay() {
     const colors = getColors();
